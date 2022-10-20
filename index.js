4244b2b8483f76b6ad1e8b701b211c2d9c4bbea4
Nisreen Hamzah <nisreenhamzah138@gmail.com>
2022-10-20 12:38:22 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// let companyName = 'Scuber';
 const companyName = 'Scuber';
+//const mostProfitableNeighborhood = 'Chelsea';
 let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
